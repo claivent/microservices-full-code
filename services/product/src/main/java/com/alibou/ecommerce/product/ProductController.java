36fa1147c01a81f9aeb1c08206357ea9084ae705
claiv <claivent@gmail.com>
2024-09-20 06:18:55 +0200

    tres

--- services/product/src/main/java/com/alibou/ecommerce/product/ProductController.java
+++ services/product/src/main/java/com/alibou/ecommerce/product/ProductController.java
@@ -3,12 +3,7 @@ package com.alibou.ecommerce.product;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +35,7 @@ public class ProductController {
         return ResponseEntity.ok(service.findById(productId));
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<ProductResponse>> findAll() {
         return ResponseEntity.ok(service.findAll());
